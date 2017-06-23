448e79375c2c0d3d68029f5797c119047ead1b52
Morebodi Modise <morebodi@codeforte.com>
2017-06-23 10:27:58 +0200

    refactored USER route

--- routes/user.js
+++ routes/user.js
@@ -1,57 +1,74 @@
+// external imports
 var router = require('express').Router();
-var Cart = require('../models/cart');
-var User = require('../models/user');
 var passport = require('passport');
-var passportConf = require('../config/passport');
 var async = require('async');
 
+// custom imports
+var Cart = require('../models/cart');
+var User = require('../models/user');
+var passportConf = require('../config/passport');
+
+/**
+ *  Handles GET HTTP requests for user login
+ */
 router.get('/login', function(req, res) {
-
+    // if user exists, then they have been logged in redirect to home page
     if (req.user) return res.redirect('/');
-
+    // user was not logged in, show error message (No user with such credentials found)
     res.render('accounts/login', { message: req.flash('loginMessage') });
-
 });
 
+/**
+ * Handles POST HTTP requests for user login - form submission
+ */
 router.post('/login', passport.authenticate('local-login', {
     successRedirect: '/profile',
     failureRedirect: '/login',
     failureFlash: true
 }));
 
+/**
+ * Handles GET HTTP requests for user profile
+ */
 router.get('/profile', passportConf.isAuthenticated, function(req, res, next) {
+    // search for a single user given the user ID
     User.findOne({ _id: req.user._id })
         .populate('history.item')
         .exec(function(err, foundUser) {
-            if (err) return next(err);
-
-            res.render('accounts/profile', { user: foundUser });
+            if (err) return next(err); // oops error might occur
+            res.render('accounts/profile', { user: foundUser }); // user found, render the profile view
         });
-})
+});
 
+/**
+ * Handles GET HTTP requests for user sign-up
+ */
 router.get('/signup', function(req, res, next) {
+    // user sign-up failed, show error message
     res.render('accounts/signup', {
         errors: req.flash('errors')
     });
 });
 
+/**
+ * Handles POST HTTP request for user sign-up
+ */
 router.post('/signup', function(req, res, next) {
-
+    // executes array of functions in series, passing result of previous function to the next
     async.waterfall([
-
+		// function 1
         function(callback) {
-            // get our mongoose model to create a new User
+            // create new user model
             var user = new User();
-
-            // populate the user properties based on what the user submits
+            // populate the user properties based on what the user submitted
             user.profile.name = req.body.name;
             user.password = req.body.password;
             user.email = req.body.email;
             user.profile.picture = user.gravatar();
 
-            // fetch user and test if they exist
+            // check submitted email against the database
             User.findOne({ email: req.body.email }, function(err, existingUser) {
-                // check if the user already exists
+                // does the user already exist?
                 if (existingUser) {
                     // return an error message to indicate user already exists
                     req.flash('errors', 'Account with that email address already exists');
@@ -60,70 +77,91 @@ router.post('/signup', function(req, res, next) {
                 } else {
                     // save the user to the database if there is no error
                     user.save(function(err, user) {
-
+						// oops error might occur
                         if (err) return next(err);
-
+						// success set user to the callback
                         callback(null, user);
                     });
                 }
             });
         },
+	    // function 2 - receives result of function 1, see line 83 above
         function(user) {
+    	// create a new cart model
             var cart = new Cart();
+            // set cart owner as the current user
             cart.owner = user._id;
+            // save cart to mongo
             cart.save(function(err) {
+            	// oops error might occur
                 if (err) return next(err);
-
+				// log user in
                 req.logIn(user, function(err) {
-
+					// error occurred
                     if (err) return next(err);
-
+					// sucess, redirect user to their profile page
                     res.redirect('/profile');
                 });
-
             });
-
         }
-
     ]);
 });
 
+/**
+ * Handles GET HTTP requests for user logout
+ */
 router.get('/logout', function(req, res, next) {
+	// terminate existing login session
     req.logout();
+    // send user back to the home/landing page
     return res.redirect('/');
 });
 
+/**
+ * Handles GET HTTP requests for editing user profile
+ */
 router.get('/edit-profile', function(req, res, next) {
+	// load the edit profile view
     res.render('accounts/edit-profile', { message: req.flash('success') });
 });
 
+/**
+ * Handles POST HTTP requests for editing user profile
+ */
 router.post('/edit-profile', function(req, res, next) {
-
+	// check submitted user id against the database
     User.findOne({ _id: req.user._id }, function(err, user) {
-
+		// error occurred
         if (err) return next(err);
-
+		// success - update user's name
         if (req.body.name) user.profile.name = req.body.name;
-
+		// update user address
         if (req.body.address) user.address = req.body.address;
-
+		// save the newly updated user details
         user.save(function(err) {
-
+			// oops error might occur
             if (err) return next(err);
-
+			// success - render success notification
             req.flash('success', 'You have successfully edited your profile information');
-
+			// redirect user to the edit profile view
             return res.redirect('/edit-profile');
         });
     });
 
 });
 
+/**
+ * Handle GET HTTP requests from Facebook authentication
+ */
 router.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
 
+/**
+ * Handles GET HTTP results from Facebook authentication
+ */
 router.get('/auth/facebook/callback', passport.authenticate('facebook', {
     successRedirect: '/profile',
     failureRedirect: '/login'
 }));
 
+// make route accessible to other files
 module.exports = router;
