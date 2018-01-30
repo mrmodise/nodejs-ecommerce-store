fe0a977985b65608dd1728c053c02b0c77c0c333
Praise <pnyama@deloitte.co.za>
2018-01-30 20:47:53 +0200

    minor refactoring the product model

--- models/product.js
+++ models/product.js
@@ -1,27 +1,20 @@
-/**
- * @author: Morebodi Modise
- * @contacts: http://github.com/mrmodise, http://mrmodise.com
- */
 var mongoose = require('mongoose')
-	require('mongoose-long')(mongoose);
-var elasticsearch = require('elasticsearch');
-
+require('mongoose-long')(mongoose);
 var mongoosastic = require('mongoosastic');
 var Schema = mongoose.Schema;
-
-var SchemaTypes = mongoose.Schema.Types;
+ObjectId = Schema.Types;
 
 var ProductSchema = new Schema({
-	category: {type: Schema.Types.ObjectId, ref: 'Category'},
-	name: String,
-	price: {
-		type: String
-	},
-	image: String
+    category: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'Category'
+    },
+    name: String,
+    price: String,
+    image: String
 });
 
 ProductSchema.plugin(mongoosastic, {
-	hosts: ['localhost:9200']
+    hosts: ['localhost:9200']
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
