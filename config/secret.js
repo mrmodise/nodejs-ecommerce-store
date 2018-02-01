3ad6e498d1cb568767cf27469b63e0a4eba69585
Praise <pnyama@deloitte.co.za>
2018-02-01 20:07:05 +0200

    .

--- config/secret.js
+++ config/secret.js
@@ -1,10 +1,10 @@
 module.exports = {
     database: 'mongodb://localhost/test',
     port: process.env.PORT || 3000,
-    secretKey: "LKSJ&%REKZ",
+    secretKey: "<secret_key>",
     facebook:{
-        clientID: process.env.FACEBOOK_ID || '186729898391051',
-        clientSecret: process.env.FACEBOOK_SECRET || 'efd0e9f14d23ad5ddfadb040db1c3066',
+        clientID: process.env.FACEBOOK_ID || '<facebook_id>',
+        clientSecret: process.env.FACEBOOK_SECRET || '<facebook_secret>',
         profileFields: ['emails','displayName'],
         callbackURL: 'http://localhost:3000/auth/facebook/callback'
     }
