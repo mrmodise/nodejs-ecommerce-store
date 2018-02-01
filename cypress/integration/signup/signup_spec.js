9cfd747f4a067b232805fdb03fb148346dec6f84
Praise <pnyama@deloitte.co.za>
2018-02-01 17:15:57 +0200

    adding more signup tests

--- cypress/integration/signup/signup_spec.js
+++ cypress/integration/signup/signup_spec.js
@@ -1,17 +1,31 @@
 // load chance library
 var Chance = require('chance');
 describe('User Registration', function () {
-    // instantiate chance
-    var chance = new Chance();
+    var chance;
+    before(function () {
+        // instantiate chance
+        chance = new Chance();
+        cy.visit('/signup');
+    });
 
     it('should register user given correct information', function () {
-        cy.visit('/signup');
-        cy.get('#signUpForm').within(function ($form) {
-            cy.get('input[name="fullName"]').type(chance.word());
+        cy.get('#signUpForm').within(function () {
+            cy.get('input[name="name"]').type(chance.word());
             cy.get('input[name="email"]').type(chance.email());
             cy.get('input[name="password"]').type('password123');
             cy.root().submit();
         });
         cy.get('#heading').should('have.text', 'History');
-    })
+    });
+
+    it('should not register an existing user', function () {
+        cy.visit('/signup');
+        cy.get('#signUpForm').within(function () {
+            cy.get('input[name="name"]').type('tester tester');
+            cy.get('input[name="email"]').type('more@gmail.com');
+            cy.get('input[name="password"]').type('password123');
+            cy.root().submit();
+        });
+        cy.get('#error-signup').should('have.text', 'Error! Account with that email address already exists');
+    });
 });
