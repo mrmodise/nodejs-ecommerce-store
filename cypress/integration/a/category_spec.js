cba38a57b44a3741d6597a47d78f5f94f576fba6
mrmodise <modisemorebodi@gmail.com>
2018-07-21 08:17:00 +0200

    refactored add category tests

--- cypress/integration/a/category_spec.js
+++ cypress/integration/a/category_spec.js
@@ -1,8 +1,21 @@
 context('Product Category', () => {
-    specify('should add food category', () => {
+
+    // TODO: refactor these tests to avoid duplicates
+    before(() => {
         cy.visit('/add-category');
+    });
+
+    specify('should add food category', () => {
         cy.get('#addCategoryForm').within(() => {
-            cy.get('#category-name').type('groceries');
+            cy.get('#category-name').type('food');
+            cy.root().submit();
+        });
+        cy.get('#cat-confirmation').should('have.text', 'Info! Successfully added a category');
+    });
+
+    specify('should add clothes category', () => {
+        cy.get('#addCategoryForm').within(() => {
+            cy.get('#category-name').type('clothes');
             cy.root().submit();
         });
         cy.get('#cat-confirmation').should('have.text', 'Info! Successfully added a category');
